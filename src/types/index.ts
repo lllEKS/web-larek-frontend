e0ac49321cce1859fc5a7f9b3726d5e39ffdd84c
Aleksey Makshakov <ras11.ru@mail.ru>
2024-10-26 12:50:10 +0500

    FinalVer1.0

---
+++ src/types/index.ts
@@ -0,0 +1,107 @@
+//Типы и интерфейсы базовых классов
+
+export type EventName = string | RegExp;
+export type Subscriber = Function;
+export type EmitterEvent = {
+	eventName: string;
+	data: unknown;
+};
+
+export interface IEvents {
+	on<T extends object>(event: EventName, callback: (data: T) => void): void;
+	emit<T extends object>(event: string, data?: T): void;
+	trigger<T extends object>(
+		event: string,
+		context?: Partial<T>
+	): (data: T) => void;
+}
+
+//Ответ от сервера
+
+export type ApiListResponse<Type> = {
+	total: number;
+	items: Type[];
+};
+
+//Запросы на сервер
+
+export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+
+//Методы для Api
+
+export interface ILarekApi {
+	getCardsList: () => Promise<ICard[]>;
+	orderProducts: (order: IOrder) => Promise<IOrderSuccess>;
+}
+
+//Интерфейсы моделей данных
+
+export interface IAppStatus {
+	catalog: ICard[];
+	basket: ICard[];
+	preview: string | null;
+	delivery: IOrdersDelivery | null;
+	contact: IOrdersContacts | null;
+	order: IOrder | null;
+}
+
+//Интерфейсы компонентов представления
+
+export interface IPage {
+	counter: number;
+	catalog: HTMLElement[];
+	locked: boolean;
+}
+
+export interface ICard {
+	id: string;
+	description: string;
+	image: string;
+	title: string;
+	category: string;
+	price: number | null;
+	count?: string;
+	buttonText?: string;
+}
+
+export interface IOrdersDelivery {
+	payment: string;
+	address: string;
+}
+
+export interface IOrdersContacts {
+	email: string;
+	phone: string;
+}
+
+export interface IOrder extends IOrdersDelivery, IOrdersContacts {
+	total: number | null;
+	items: string[];
+}
+
+export interface IOrderSuccess {
+	id: string;
+	total: number | null;
+}
+
+export interface ISuccess {
+	image: string;
+	title: string;
+	description: string;
+	total: number | null;
+}
+
+export interface IBasket {
+	items: HTMLElement[];
+	total: number;
+}
+
+export type FormErrors = Partial<Record<keyof IOrder, string>>;
+
+export interface IActions {
+	onClick: (event: MouseEvent) => void;
+}
+
+export interface ISuccessActions {
+	onClick: () => void;
+}
